290caa4681914f7decdc3159d9c4147f94b23588
PaperSupe <thepapersupe@gmail.com>
2026-01-03 09:21:29 +0000

    loguru fix

---
+++ backend/config/logging.py
@@ -0,0 +1,10 @@
+from loguru import logger
+import sys
+
+def setup_logging():
+    logger.remove()
+    logger.add(
+        sys.stdout,
+        level="INFO",
+        format="<green>{time}</green> | <level>{level}</level> | {message}"
+    )
